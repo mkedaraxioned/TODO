e3b59bbf40bc1c51b35c99b4b17cd993575043d1
kedarmayekar <kedarmayekar077@gmail.com>
2021-08-06 15:09:01 +0530

    Authorized user information now added to notes during its creation

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -2,13 +2,14 @@ const Note = require('./../models/Note');
 class NoteController {
   addNote = async (req,res,next) => {
     try {
-      if(new Date(req.body.onDate) <= new Date()) return res.status(403).send({error:'Past dates cannot be added'});       
+      if(new Date(req.body.onDate) <= new Date()) return res.status(403).send({error:'Please enter a valid date'});       
       const note = {
         content: req.body.content,
         status:req.body.status,
-        onDate: req.body.onDate
+        onDate: req.body.onDate,
+        user: res.locals.userEmail
       };
-      const newNote = await Note.create(req.body);
+      const newNote = await Note.create(note);
       return res.status(201).send({message:'Note added Successfully',note:newNote});
       } catch(error) { 
       next(error);
